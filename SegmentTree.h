9f7b6dd5a6e841bff6f984ab5084b8dd416c4504
Mohamed Amir <me.mohamed.amir@gmail.com>
2024-01-04 23:05:58 +0200

    init commit

---
+++ SegmentTree.h
@@ -0,0 +1,10 @@
+# ifndef SEGMENTTREE_SEGMENTTREE_H
+# define SEGMENTTREE_SEGMENTTREE_H
+# include <iostream>
+using namespace std;
+
+class SegmentTree {
+
+};
+
+# endif //SEGMENTTREE_SEGMENTTREE_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+# include "SegmentTree.h"
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
